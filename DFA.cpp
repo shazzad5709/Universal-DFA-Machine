00985cb7c357f650fc1f5b77414c5975f48d7f0d
shazzad5709 <62200344+shazzad5709@users.noreply.github.com>
2022-03-02 10:39:09 +0600

    added input symbol (not tested)

--- DFA.cpp
+++ DFA.cpp
@@ -3,6 +3,12 @@ using namespace std;
 
 int main()
 {
+    cout<<"No. of Input Symbols: ";
+    int n;
+    cin>>n;
+    n--;
+    //let the alphabet be {0, 1, 2, 3, ....., n-1}
+    
     cout<<"No. of states: ";
     int states;
     cin>>states;
@@ -29,15 +35,16 @@ int main()
         check[f[i]]=true;
     }
 
-    int tt[states][2];
+    int tt[states][n];
     cout<<"-------------------------\n";
     cout<<"State Transitions: "<<endl;
     for(int i=0; i<states; i++)
     {
-        cout<<"(q"<<i<<", 0)=q";
-        cin>>tt[i][0];
-        cout<<"(q"<<i<<", 1)=q";
-        cin>>tt[i][1];
+        for(j=0; j<=n; j++)
+        {
+            cout<<"(q"<<i<<", j)=q";
+            cin>>tt[i][j];
+        }
     }
     cout<<"-------------------------\n";
 
